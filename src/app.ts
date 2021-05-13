14319b0d7cabb925f38033a4f152492ea648045b
Liber <libermenna@gmail.com>
2021-05-13 20:43:15 -0300

    ADD: cors config

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 initSocketServer(app);
 setClientSocketEvents();
 
